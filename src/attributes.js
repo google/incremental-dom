ce2f4d492adfe03e8e44a3cad6f6473e166251eb
aimwhy <aimwhy@foxmail.com>
2016-03-22 22:13:02 +0800

    Update attributes.js

--- src/attributes.js
+++ src/attributes.js
@@ -101,16 +101,16 @@ const applyAttributeTyped = function(el, name, value) {
  */
 const updateAttribute = function(el, name, value) {
   const data = getData(el);
-  const attrs = data.attrs;
+  const attrMap = data.attrMap;
 
-  if (attrs[name] === value) {
+  if (attrMap[name] === value) {
     return;
   }
 
   const mutator = attributes[name] || attributes[symbols.default];
   mutator(el, name, value);
 
-  attrs[name] = value;
+  attrMap[name] = value;
 };
 
 
