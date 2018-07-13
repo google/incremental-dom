584cbe71223f933030ed54d92dc702027f6829b5
Sepand Parhami <sparhami@google.com>
2018-07-13 16:40:46 -0700

    Add a demo for animating items while reordering.
    - Not a part of Incremental DOM per se but show how Incremental DOM just
    modifies DOM and you can use a MutationObserver to do a animation when
    changing the order of items.

--- demo/reorder/reorder_list.js
+++ demo/reorder/reorder_list.js
@@ -1,62 +1,62 @@
 // Based on https://github.com/joshwcomeau/react-flip-move/blob/master/src/FlipMove.js
-const reorderList = (function() {
-  const elementOpen = IncrementalDOM.elementOpen;
-  const elementClose = IncrementalDOM.elementClose;
-  const currentElement = IncrementalDom.currentElement;
+const {
+  elementOpen,
+  elementClose,
+  currentElement,
+} = IncrementalDOM;
 
-  const locations = new WeakMap();
-  const initializedEls = new WeakSet();
+const locations = new WeakMap();
+const initializedEls = new WeakSet();
 
-  function updateOffset(node) {
-    const rect = node.getBoundingClientRect();
-    const lastLocation = locations.get(node);
-    const newLocation = {
-      top: rect.top
-    };
+function updateOffset(node) {
+  const rect = node.getBoundingClientRect();
+  const lastLocation = locations.get(node);
+  const newLocation = {
+    top: rect.top
+  };
 
-    locations.set(node, newLocation);
+  locations.set(node, newLocation);
 
-    if (!lastLocation) {
-      return;
-    }
-
-    const deltaY = lastLocation.top - newLocation.top;
-    node.style.transitionDuration = '0ms';
-    node.style.transform = `translate(0, ${deltaY}px)`;
+  if (!lastLocation) {
+    return;
   }
 
-  function triggerAnimation(nodes) {
+  const deltaY = lastLocation.top - newLocation.top;
+  node.style.transitionDuration = '0ms';
+  node.style.transform = `translate(0, ${deltaY}px)`;
+}
+
+function triggerAnimation(nodes) {
+  requestAnimationFrame(_ => {
     requestAnimationFrame(_ => {
-      requestAnimationFrame(_ => {
-        nodes.forEach(node => {
-          node.style.transitionDuration = '';
-          node.style.transform = '';
-        });
+      nodes.forEach(node => {
+        node.style.transitionDuration = '';
+        node.style.transform = '';
       });
     });
+  });
+}
+
+function attachList(el) {
+  if (initializedEls.has(el)) {
+    return;
   }
 
-  function attachList(el) {
-    if (initializedEls.has(el)) {
-      return;
-    }
+  new MutationObserver(_ => {
+    const children = [...el.childNodes];
+    children.forEach(updateOffset);
+    triggerAnimation(children);
+  }).observe(el, {childList: true});
 
-    new MutationObserver(_ => {
-      const children = [].slice.call(el.childNodes);
-      children.forEach(updateOffset);
-      triggerAnimation(children);
-    }).observe(el, { childList: true });
+  initializedEls.add(el);
+}
 
-    initializedEls.add(el);
-  }
-
-  return function(fn) {
-    // Treating element as a component, so it should have a custom tag so
-    // that the mutation observer we create is never reused with a different
-    // logical element.
-    elementOpen('x-reorderlist');
-      attachList(currentElement());
-      fn();
-    elementClose('x-reorderlist');
-  };
-})();
+export function reorderList(fn) {
+  // Treating element as a component, so it should have a custom tag so
+  // that the mutation observer we create is never reused with a different
+  // logical element.
+  elementOpen('x-reorderlist');
+    attachList(currentElement());
+    fn();
+  elementClose('x-reorderlist');
+};
