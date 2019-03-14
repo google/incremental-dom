bca8fd4305c2a157f83e35500d3293b47934bb01
Thomas Nguyen <tomnguyen@google.com>
2019-03-13 18:37:37 -0700

    fix spacing:

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@
 export {applyAttr, applyProp, attributes,} from './src/attributes';
 export {alignWithDOM, close, currentElement, currentPointer, open, patchInner as patch, patchInner, patchOuter, skip, skipNode} from './src/core';
 export {setKeyAttributeName} from './src/global';
-export { clearCache,getKey, importNode, isDataInitialized} from './src/node_data';
+export {clearCache,getKey, importNode, isDataInitialized} from './src/node_data';
 export {notifications} from './src/notifications';
 export {symbols} from './src/symbols';
 export {attr, elementClose, elementOpen, elementOpenEnd, elementOpenStart, elementVoid, key, text} from './src/virtual_elements';
