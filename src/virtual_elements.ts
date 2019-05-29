d4fa43c6896af9a03d12651026dd457a72ff5596
SLaks <slaks@google.com>
2019-05-29 16:35:15 -0400

    Don't set node.data if it hasn't changed.
    See https://github.com/patrick-steele-idem/morphdom/pull/100; this is caused by https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/11375311/

--- src/virtual_elements.ts
+++ src/virtual_elements.ts
@@ -360,7 +360,8 @@ function text(value: string|number|boolean, ...varArgs: Array<(a: {}) => string>
       formatted = fn(formatted);
     }
 
-    node.data = formatted as string;
+    // Setting node.data resets the cursor in IE/Edge.
+    if (node.data !== formatted) node.data = formatted as string;
   }
 
   return node;
