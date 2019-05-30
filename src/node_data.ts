3165a227641be2801f1c5d0ed7cb2b76362f8995
SLaks <Schabse@GMail.com>
2019-05-30 14:17:28 -0400

    Don't set node.data if it hasn't changed. (#398)
    * Don't set node.data if it hasn't changed.
      - See https://github.com/patrick-steele-idem/morphdom/pull/100; this is caused by https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/11375311/
    * Test that text nodes aren't updated to same value
    * Don't import text nodes text as initial data
      - Since they may not match pre-formatted data.

--- src/node_data.ts
+++ src/node_data.ts
@@ -15,10 +15,10 @@
  * limitations under the License.
  */
 
-import {getKeyAttributeName} from './global';
-import {Key, NameOrCtorDef} from './types';
 import {assert} from './assertions';
 import {isElement, isText} from './dom_util';
+import {getKeyAttributeName} from './global';
+import {Key, NameOrCtorDef} from './types';
 
 
 /**
@@ -77,7 +77,8 @@ declare global {
  * Initializes a NodeData object for a Node.
  */
 function initData(
-    node: Node, nameOrCtor: NameOrCtorDef, key: Key, text?: string|undefined): NodeData {
+    node: Node, nameOrCtor: NameOrCtorDef, key: Key,
+    text?: string|undefined): NodeData {
   const data = new NodeData(nameOrCtor, key, text);
   node['__incrementalDOMData'] = data;
   return data;
@@ -110,10 +111,10 @@ function importSingleNode(node: Node, fallbackKey?: Key) {
   const nodeName = isElement(node) ? node.localName : node.nodeName;
   const keyAttrName = getKeyAttributeName();
   const keyAttr = isElement(node) && keyAttrName != null ?
-      node.getAttribute(keyAttrName) : null;
+      node.getAttribute(keyAttrName) :
+      null;
   const key = isElement(node) ? keyAttr || fallbackKey : null;
-  const text = isText(node) ? node.data : undefined;
-  const data = initData(node, nodeName!, key, text);
+  const data = initData(node, nodeName!, key);
 
   if (isElement(node)) {
     recordAttributes(node, data);
@@ -128,7 +129,8 @@ function importSingleNode(node: Node, fallbackKey?: Key) {
 function importNode(node: Node) {
   importSingleNode(node);
 
-  for (let child: Node|null = node.firstChild; child; child = child.nextSibling) {
+  for (let child: Node|null = node.firstChild; child;
+       child = child.nextSibling) {
     importNode(child);
   }
 }
@@ -139,7 +141,8 @@ function importNode(node: Node) {
 function clearCache(node: Node) {
   node['__incrementalDOMData'] = null;
 
-  for (let child: Node|null = node.firstChild; child; child = child.nextSibling) {
+  for (let child: Node|null = node.firstChild; child;
+       child = child.nextSibling) {
     clearCache(child);
   }
 }
